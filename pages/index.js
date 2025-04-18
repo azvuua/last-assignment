9c35eba6a7af9a567ab5cfe6702ae5df70f8d5e0
einnew <einnew@einnews-Laptop.local>
2025-04-18 10:51:58 +0800

    o

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
-import { useRouter } from "next/router";
+import React from "react";
 
 export default function Home() {
-  const router = useRouter();
+
+  React.useEffect(() => {
+    document.querySelectorAll("[data-scroll-to]").forEach((item) => {
+      item.addEventListener("click", () => {
+        const targetId = item.getAttribute("data-scroll-to");
+        const targetElement = document.querySelector(targetId);
+ 
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+        } else {
+          console.error(`Element with ID ${targetId} not found`);
+        }
+      });
+    });
+  }, []);
+
 
   return (
-    <div className="w-full h-[4400px] pt-0 bg-white">
+    <div id="ehlel" className="w-full h-[4400px] pt-0 bg-white">
       <div className="flex w-full h-20 p-4 fixed top-0 left-0 bg-white z-50 shadow">
-        <img
+        <img data-scroll-to="#ehlel"
           className="h-12 ml-64"
           src="https://www.toki.mn/wp-content/uploads/2022/08/Asset-1@3x.png"
           alt="Logo"
         />
-        <p className="p-3 text-gray-900 ml-[500px]">Үйлчилгээ</p>
-        <p className="p-3 text-gray-900">Давуу тал</p>
-        <p className="p-3 text-gray-900">Мэдээ</p>
-        <p className="p-3 text-gray-900">Хамтран ажиллах</p>
+        <button className="p-3 text-gray-900 ml-[500px]" data-scroll-to="#section1">Үйлчилгээ</button>
+        <button data-scroll-to="#section2" className="p-3 text-gray-900">Давуу тал</button>
+        <button data-scroll-to="#section3" className="p-3 text-gray-900">Мэдээ</button>
+        <button data-scroll-to="#section4" className="p-3 text-gray-900">Хамтран ажиллах</button>
       </div>
 
       <div className="h-[1000px] flex flex-col justify-center items-center pt-4 bg-yellow-100 mt-20">
@@ -41,8 +56,8 @@ export default function Home() {
 
 
 
-      <div className="h-[800px] flex flex-row p-10 bg-white mt-20">
-        <div className="ml-20">
+      <div id="section1"  className="h-[800px]  flex items-center justify-center flex-row bg-white mt-20">
+        <div className="ml-20 ">
           <img
             className="h-[700px]"
             src="/lab.jpeg"
@@ -50,7 +65,7 @@ export default function Home() {
           />
         </div>
 
-        <div className="ml-10 w-[600px]  right-20 flex flex-col justify-start mt-52">
+        <div className="ml-10 w-[600px] right-20 flex flex-col justify-start mt-52">
           <p className="text-yellow-500  text-3xl font-bold mb-4">Toki Service</p>
           <p className="text-black text-lg font-medium ">
             Та такси дуудах, кофе, хоол захиалах, нэгж дата авах, хэрэглээний
@@ -60,7 +75,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="h-[800px] flex flex-row p-10 bg-white mt-20">
+      <div id="section3" className="h-[800px] pt- flex flex-row p-10 bg-white mt-20" >
         
 
         <div className="ml-40 w-[500px]  right-20 flex flex-col justify-start mt-52">
@@ -82,7 +97,7 @@ export default function Home() {
 
 
 
-      <div className="h-[800px] flex flex-row p-10 bg-white mt-20">
+      <div id="section2" className="h-[800px] flex flex-row p-10 bg-white mt-20">
         <div className="ml-52">
           <img
             className="h-[700px]"
@@ -91,8 +106,8 @@ export default function Home() {
           />
         </div>
 
-        <div className="ml-10 w-[500px]  right-20 flex flex-col justify-start mt-60">
-          <p className="text-yellow-500  text-3xl font-bold mb-4">Toki Service</p>
+        <div id="section2" className="ml-10 w-[500px]   right-20 flex flex-col justify-start mt-60">
+          <p className="text-yellow-500  text-3xl font-bold mb-4">Toki Shop</p>
           <p className="text-black text-lg font-medium ">
           Та өөрийн хүссэн бараагаа албан ёсны дэлгүүрүүдээс сонголт хийж, нэг дороос шууд захиалах боломжтой.
           </p>
@@ -100,7 +115,7 @@ export default function Home() {
       </div>
 
 
-      <div className="ml-10 flex flex-col w-full right-20 items-center justify-center">
+      <div className="ml-10 flex flex-col w-full right-20 pt-40 items-center justify-center">
           <p className="text-yellow-500  text-3xl font-bold mb-4">Toki апп өдөр бүр танд тусална</p>
           <p className="text-black text-lg font-medium mt-10 ">
           Toki апп бол хүн бүрийн өдөр тутамд хэрэгтэй бүх үйлчилгээг нэг дор багтаасан таны цаг хугацааг хэмнэх хялбар, хэрэгтэй аппликейшн юм.
@@ -110,7 +125,7 @@ export default function Home() {
         <div className="w-full mt-40 h-40 bg-yellow-100 flex justify-center items-center">
           <button className="rounded-full text-white bg-yellow-400 p-3 pl-16 pr-16">Давуу тал</button>
         </div>
-        <div className="bg-gray-900 h-[350px] flex justify-center items-center text-gray-400 ">
+        <div id="section4" className="bg-gray-900 h-[350px] flex justify-center items-center text-gray-400 " >
             <img className="h-10 " src="https://www.toki.mn/wp-content/uploads/2022/08/Asset-1@3x.png"></img>
             <div className="ml-40">
               <p className="h-2 p-3">Түгээмэл асуулт</p>
